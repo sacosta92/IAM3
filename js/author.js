a0dcd65cbc6b97c3822d7413db91d452800144bf
Stephanie <sa9two@yahoo.com>
2014-11-28 11:32:03 -0800

    use jquery mobile

--- js/author.js
+++ js/author.js
@@ -5,7 +5,7 @@ function phonegapLoaded() {
 
 }
 function takePicture() {
-    navigator.camera.getPicture(onSuccess,onFail,{ quality: 70, destinationType: Camera.DestinationType.FILE_URI, sourceType: Camera.PictureSourceType.PHOTOLIBRARY});
+    navigator.camera.getPicture(onSuccess,onFail,{ quality: 70, destinationType: Camera.DestinationType.FILE_URI, sourceType: Camera.PictureSourceType.PHOTOLIBRARY, encodingType: Camera.EncodingType.JPEG});
 }
 function onSuccess(imageURI) {
     var image = document.getElementById('image');
@@ -14,4 +14,13 @@ function onSuccess(imageURI) {
 }
 function onFail(message) {
     alert('Failed because: ' + message);
+}
+function() {
+	private void galleryAddPic() {
+	    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+	    File f = new File(mCurrentPhotoPath);
+	    Uri contentUri = Uri.fromFile(f);
+	    mediaScanIntent.setData(contentUri);
+	    this.sendBroadcast(mediaScanIntent);
+	}
 }
