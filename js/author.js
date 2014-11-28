87b4261baef79415dcada9e0b8697f8a658d2814
Stephanie <sa9two@yahoo.com>
2014-11-28 11:40:58 -0800

    try for libary and camera links

--- js/author.js
+++ js/author.js
@@ -7,6 +7,9 @@ function phonegapLoaded() {
 function takePicture() {
     navigator.camera.getPicture(onSuccess,onFail,{ quality: 70, destinationType: Camera.DestinationType.FILE_URI, sourceType: Camera.PictureSourceType.PHOTOLIBRARY, encodingType: Camera.EncodingType.JPEG});
 }
+function takeCamera() {
+    navigator.camera.getPicture(onSuccess,onFail,{ quality: 70, destinationType: Camera.DestinationType.FILE_URI, sourceType: Camera.PictureSourceType.CAMERA, encodingType: Camera.EncodingType.JPEG});
+}
 function onSuccess(imageURI) {
     var image = document.getElementById('image');
     image.src = imageURI;
