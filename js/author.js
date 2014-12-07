dab8322755e6603e43c4ee7b998d225b1234c1ed
Stephanie <sa9two@yahoo.com>
2014-12-06 22:05:17 -0800

    create seperate stylesheet

--- js/author.js
+++ js/author.js
@@ -18,10 +18,48 @@ function onSuccess(imageURI) {
 function onFail(message) {
     alert('Failed because: ' + message);
 }
-// private void galleryAddPic() {
-//     Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-//     File f = new File(mCurrentPhotoPath);
-//     Uri contentUri = Uri.fromFile(f);
-//     mediaScanIntent.setData(contentUri);
-//     this.sendBroadcast(mediaScanIntent);
-// }
+
+
+
+
+
+
+/*public static final int MEDIA_TYPE_IMAGE = 1;
+public static final int MEDIA_TYPE_VIDEO = 2;
+
+* Create a file Uri for saving an image or video 
+private static Uri getOutputMediaFileUri(int type){
+      return Uri.fromFile(getOutputMediaFile(type));
+}*/
+
+/** Create a File for saving an image or video */
+/*private static File getOutputMediaFile(int type){
+    // To be safe, you should check that the SDCard is mounted
+    // using Environment.getExternalStorageState() before doing this.
+
+    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+              Environment.DIRECTORY_PICTURES), "OverlayEditApp");
+    // This location works best if you want the created images to be shared
+    // between applications and persist after your app has been uninstalled.
+
+    // Create the storage directory if it does not exist
+    if (! mediaStorageDir.exists()){
+        if (! mediaStorageDir.mkdirs()){
+            Log.d("OverlayEditApp", "failed to create directory");
+            return null;
+        }
+    }
+
+    // Create a media file name
+    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    File mediaFile;
+    if (type == MEDIA_TYPE_IMAGE){
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+        "IMG_"+ timeStamp + ".jpg");
+    } 
+    } else {
+        return null;
+    }
+
+    return mediaFile;
+}*/
